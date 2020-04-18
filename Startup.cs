2ea93b9907ad1b9365e44b83b52599076ce7a52c
LeblancJustin <54998480+LeblancJustin@users.noreply.github.com>
2020-04-18 17:16:13 -0400

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrokenAuthentication_Demo.Models.Contexte;
+using BrokenAuthentication_Demo.Models.Enttities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrokenAuthentication_Demo
+{
+    public class Startup
+    {
+        IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ContexteApplication>(
+                options => options.UseSqlServer(
+                    Configuration["Donnees:ConnexionData:ChaineDeConnexion"]));
+            services.AddSingleton<IDepot, Depot>();
+
+            services.AddSession();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseSession();
+
+            AjoutDonnes.CréerDonnées(app);
+
+        }
+    }
+}
